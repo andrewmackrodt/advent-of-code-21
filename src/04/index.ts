3f3d97e48d36f921ac834dc31abb413d3144c351
Andrew Mackrodt <andrewmackrodt@gmail.com>
2021-12-08 19:46:33 +0000

    Add Day 04 - Part Two

--- src/04/index.ts
+++ src/04/index.ts
@@ -1,9 +1,9 @@
 import { readFileSync } from 'fs'
 
-export function solve(input: string): number {
+function* generator(input: string): Generator<number> {
     const lines = input.split('\n\n')
     const picks = lines.shift()!.split(',').map(s => parseInt(s, 10))
-    const boards = lines.map(board => (
+    let boards = lines.map(board => (
         board.split('\n')
             .map(line => line.split(/ +/)
                 .filter(s => s.length > 0)
@@ -11,27 +11,35 @@ export function solve(input: string): number {
             )
     ))
 
-    // HADOKEN ===D
-    for (const pick of picks) {
-        for (const board of boards) {
-            for (let y = 0; y < board.length; y++) {
-                const line = board[y]
-                for (let x = 0; x < line.length; x++) {
-                    const num = line[x]
-                    if (num === pick) {
-                        line[x] = NaN
-                        if (
-                            line.filter(n => ! isNaN(n)).length === 0 ||
-                            board.map(line => line[x]).filter(n => ! isNaN(n)).length === 0
-                        ) {
-                            const sumUnmarked = board.flat()
-                                .filter(n => ! isNaN(n))
-                                .reduce((res, cur) => res + cur, 0)
+    const markNumberAndGetScoreIfBoardComplete = (pick: number, board: number[][]): number | undefined => {
+        // HADOKEN =D
+        for (let y = 0; y < board.length; y++) {
+            const line = board[y]
+            for (let x = 0; x < line.length; x++) {
+                const num = line[x]
+                if (num === pick) {
+                    line[x] = NaN
+                    if (
+                        line.filter(n => ! isNaN(n)).length === 0 ||
+                        board.map(line => line[x]).filter(n => ! isNaN(n)).length === 0
+                    ) {
+                        const sumUnmarked = board.flat()
+                            .filter(n => ! isNaN(n))
+                            .reduce((res, cur) => res + cur, 0)
 
-                            return sumUnmarked * pick
-                        }
+                        return sumUnmarked * pick
                     }
                 }
+            }
+        }
+    }
+
+    for (const pick of picks) {
+        for (const board of boards) {
+            const score = markNumberAndGetScoreIfBoardComplete(pick, board)
+            if (typeof score === 'number') {
+                boards = boards.filter(b => b !== board)
+                yield score
             }
         }
     }
@@ -39,9 +47,22 @@ export function solve(input: string): number {
     return 0
 }
 
+export function getWinningBoardScore(input: string): number {
+    return generator(input).next().value
+}
+
+export function getLosingBoardScore(input: string): number {
+    let lastScore = 0
+    for (const winningScore of generator(input)) {
+        lastScore = winningScore
+    }
+    return lastScore
+}
+
 // print solution to terminal if invoked directly
 if (require.main === module) {
     const input = readFileSync(__dirname + '/input.txt').toString()
 
-    console.log(solve(input))
+    console.log(getWinningBoardScore(input))
+    console.log(getLosingBoardScore(input))
 }
