c0911557f10bad17dbd86a361cd968ccd90c234a
Andrew Mackrodt <andrewmackrodt@gmail.com>
2021-12-10 00:11:30 +0000

    Add Day 07 - Part Two

--- src/07/index.ts
+++ src/07/index.ts
@@ -1,5 +1,9 @@
 import { readFileSync } from 'fs'
 
+interface Options {
+    burn?: 'constant' | 'linear'
+}
+
 class FuelCalculator {
     private readonly positions: Record<number, number> = {}
     private min = Number.MAX_SAFE_INTEGER
@@ -17,43 +21,51 @@ class FuelCalculator {
         return instance
     }
 
-    public getMinimumUsage(): number {
-        const positions = Object.keys(this.positions).map(n => parseInt(n, 10))
+    public getMinimumUsage(options?: Options): number {
+        if ( ! options?.burn) console.warn('calling getMinimumUsage without specifying options.burn is deprecated')
 
-        while (positions.length > 1) {
-            const midIndex = Math.floor(positions.length / 2)
+        let left = this.min
+        let right = this.max
 
-            if (positions.length % 2 === 0) {
+        for (let diff = right - left; diff > 0; diff = right - left) {
+            const midIndex = (left + right) / 2
+
+            if (midIndex % 2 === 0) {
                 const midLeftIndex = midIndex - 1
                 const midRightIndex = midIndex
                 // move left
-                if (this.getUsage(positions[midLeftIndex]) < this.getUsage(positions[midRightIndex])) {
-                    positions.splice(midLeftIndex + 1)
+                if (this.getUsage(midLeftIndex, options) < this.getUsage(midRightIndex, options)) {
+                    right = midLeftIndex + 1
                 }
                 // move right
                 else {
-                    positions.splice(0, midRightIndex)
+                    left = midRightIndex
                 }
             } else {
                 // move left
-                if (this.getUsage(positions[0]) < this.getUsage(positions[positions.length - 1])) {
-                    positions.splice(midIndex + 1)
+                if (this.getUsage(left, options) < this.getUsage(right, options)) {
+                    right = Math.floor(midIndex)
                 }
                 // move right
                 else {
-                    positions.splice(0, midIndex)
+                    left = Math.ceil(midIndex)
                 }
             }
         }
 
-        return this.getUsage(positions[0])
+        return this.getUsage(left, options)
     }
 
-    private getUsage(target: number) {
+    private getUsage(target: number, options?: Options) {
         let moves = 0
         for (const key in this.positions) {
             const position = parseInt(key, 10)
-            const diff = Math.abs(target - position)
+            let diff = Math.abs(target - position)
+            if (options?.burn === 'linear') {
+                for (let i = 1, len = diff; i < len; i++) {
+                    diff += i
+                }
+            }
             if (diff > 0) {
                 moves += diff * this.positions[key]
             }
@@ -62,10 +74,10 @@ class FuelCalculator {
     }
 }
 
-export function solve(input: string): number {
+export function solve(input: string, options?: Options): number {
     const fuelCalculator = FuelCalculator.createFromString(input)
 
-    return fuelCalculator.getMinimumUsage()
+    return fuelCalculator.getMinimumUsage(options)
 }
 
 // print solution to terminal if invoked directly
@@ -73,4 +85,5 @@ if (require.main === module) {
     const input = readFileSync(__dirname + '/input.txt').toString()
 
     console.log(solve(input))
+    console.log(solve(input, { burn: 'linear' }))
 }
