c88e47289401746030c744ca6e16981917e50d58
Andrew Mackrodt <andrewmackrodt@gmail.com>
2021-12-14 02:18:49 +0000

    Add Day 09

---
+++ src/09/index.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from 'fs'
+
+export function solve(input: string): number {
+    const heightmap = input.split('\n').map(row => row.split('').map(cell => parseInt(cell, 10)))
+    const lowest: number[] = []
+
+    for (let y = 0; y < heightmap.length; y++) {
+        const row = heightmap[y]
+        for (let x = 0; x < row.length; x++) {
+            const value = heightmap[y][x]
+            const neighbours: number[] = []
+            if (y > 0)                    neighbours.push(heightmap[y - 1][x]) // top
+            if (x < row.length - 1)       neighbours.push(heightmap[y][x + 1]) // right
+            if (y < heightmap.length - 1) neighbours.push(heightmap[y + 1][x]) // bottom
+            if (x > 0)                    neighbours.push(heightmap[y][x - 1]) // left
+            const higher = neighbours.filter(n => n > value)
+            if (higher.length === neighbours.length) {
+                lowest.push(value)
+            }
+        }
+    }
+
+    return lowest.reduce((res, cur) => res + cur + 1, 0)
+}
+
+// print solution to terminal if invoked directly
+if (require.main === module) {
+    const input = readFileSync(__dirname + '/input.txt').toString()
+
+    console.log(solve(input))
+}
